95327b2dbb0173069d2bbc79a7e218b119777a1e
Chris Maddox <tyre77@gmail.com>
2023-08-03 00:43:46 +0200

    remove unnecessary function

--- src/vectors/metadata_builders/SimpleMetadataBuilder.ts
+++ src/vectors/metadata_builders/SimpleMetadataBuilder.ts
@@ -20,9 +20,13 @@ export class SimpleMetadataBuilder implements PineconeMetadataBuilder {
         return metadata;
       } else if (typeof key !== "string") {
         throw new Error(`Metadata key ${key} must be a string`);
+
+        // If it's an object, throw an error.
       } else if (isAnObject(value)) {
         throw new Error(`Metadata value for ${key} cannot be an object`);
-      } else if (isAnArray(value)) {
+
+        // If it's an array, make sure there are no objects in it.
+      } else if (Array.isArray(value)) {
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         value.forEach((arrayValue: any) => {
           if (isAnObject(arrayValue)) {
@@ -37,12 +41,8 @@ export class SimpleMetadataBuilder implements PineconeMetadataBuilder {
   }
 }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-function isAnArray(value: any): boolean {
-  return Array.isArray(value);
-}
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function isAnObject(value: any): boolean {
-  return typeof value === "object" && !isAnArray(value);
+  return typeof value === "object" && !Array.isArray(value);
 }
